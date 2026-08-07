96ca1cdf83597cd4b0f23753a562771404593d05
M.Durga Praneeth <muvvalapraneeth@gmail.com>
2026-08-07 20:09:44 +0530

    Fix indentation and formatting in 1.php

--- 1.php
+++ 1.php
@@ -10,7 +10,6 @@ if (isset($_POST['submit'])) {
     $result = mysqli_query($conn, $query);
     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
     $count = mysqli_num_rows($result);
-
     if ($count == 1) {
         echo "<script>window.location='studentdat.php';</script>";
     } else {
