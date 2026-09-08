b9206fc1c0c4a385d2cc55f7b5fdac0e90c4256c
Praneeth3105 <muvvalapraneeth@gmail.com>
2026-09-08 14:55:30 +0530

    sewe

--- sewe.php
+++ sewe.php
@@ -84,7 +84,6 @@ select{
     $_SESSION['ie'] = $_POST['ie'];
     $_SESSION['ay'] = $_POST['ay'];
 $_SESSION['acc'] = $_POST['acc'];
-
     $filename = $_FILES["file"]["name"];
     $tempname = $_FILES["file"]["tmp_name"];
     $folder = "images/".$filename;  
